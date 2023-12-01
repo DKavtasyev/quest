9f8a079a650f803fae859dab9c1bcfe638605f66
Dmitriy Kavtasyev <dkavtasyev@gmail.com>
2023-12-01 09:47:01 +0400

    Fix of remarks

--- src/main/java/com/javarush/quest/kavtasyev/filters/NonAuthorizedUserFilter.java
+++ src/main/java/com/javarush/quest/kavtasyev/filters/NonAuthorizedUserFilter.java
@@ -7,8 +7,6 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
